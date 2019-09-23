d791bffe581ee8984efc3395881722dd53204adf
Adejobi Ezekiel <31517324+Ezeko@users.noreply.github.com>
2019-09-24 00:48:16 +0100

    add connect.php add css folder add signup html add signupauth.php

---
+++ signupauth.php
@@ -0,0 +1,14 @@
+<?php
+
+//connect database
+require_once('connect.php');
+
+//get data from form
+$fullname =$_POST['fullname'];
+$username =$_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$password2 = $_POST['password2'];
+
+
+?>
