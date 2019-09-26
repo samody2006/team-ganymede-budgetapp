50858b3c10dfb436a6cd8a06562f7674be6d4c34
yanmifeakeju <yanmifeakeju@gmail.com>
2019-09-26 20:33:11 +0100

    files modified for login/signup authentification. Do not tamper with php files.

--- connect.php
+++ connect.php
@@ -1,16 +1,16 @@
 <?php
 //database connection
-$db_host = "localhost";
-$db_user = "root";
-$db_password = "";
-$db_name = "budget_app_hngi6";
+$db_host = "sql101.epizy.com";
+$db_user = "epiz_24542305";
+$db_password = "BfxDnqQK4RWUa";
+$db_name = "epiz_24542305_ganymede";
 
 
 $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 if (!$conn){
     echo ("database not connected". mysqli_error($conn));
 }else{
-    //echo(" database connected");
+    echo(" database connected");
 }
 
 ?>
