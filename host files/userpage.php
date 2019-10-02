2e5d6c24daa353d05274b02cfc2fe4070326608b
Adejobi Ezekiel <31517324+Ezeko@users.noreply.github.com>
2019-10-02 22:05:51 +0100

    make hamburger function

--- host files/userpage.php
+++ host files/userpage.php
@@ -117,6 +117,7 @@ if(isset($_POST['submit'])){
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/1129efb8ac.js"></script>      
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> 
+    
     <style>
          .col-8{
             padding-left: 5%;
@@ -170,6 +171,10 @@ if(isset($_POST['submit'])){
                 padding-left: 5%;
                 font-size: 100%;
             }
+        }
+
+        input[type='number']{
+            width: 100%;
         }
 
     
@@ -277,7 +282,7 @@ if(isset($_POST['submit'])){
         </div>
     </div>
     
-    
+    <script src="./js/menu-action.js"></script>
 <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     
@@ -310,6 +315,6 @@ if(isset($_POST['submit'])){
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="./js/menu-action.js"></script>
+    
 </body>
 </html>
